75dd1b6c77b1d55f14dea2f6a686847609c81166
PSYAMEN <nicolas.2.brebisson@gmail.com>
2026-07-12 03:37:43 +0200

    rework for a few of the scripts

--- rofi/scripts/themes.sh
+++ rofi/scripts/themes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 folder="/home/$USER/.config/hyprlandThemes"
-mapfile -t themes < <(find "$folder" -type f \( -iname "*.conf" \) -printf "%f\t%p\n")
+mapfile -t themes < <(find "$folder" -mindepth 1 -type d -printf "%f\t%p\n")
 selected=$(printf '%s\n' "${themes[@]}" | cut -f1 | rofi -dmenu -theme ~/.config/rofi/themes/blue2.rasi -i -p "Themes")
 if [ -n "$selected" ]; then
   full_path=$(printf '%s\n' "${themes[@]}" | grep -F "$selected" | cut -f2 -d$'\t')
-  sed -i '1s|.*|source= '"$full_path"'|' ~/.config/hypr/hyprland.conf 
+  sed -i '1s|.*|source= '"${full_path}.conf"'|' ~/.config/hypr/hyprland.conf 
   hyprctl reload
   killall waybar
-  waybar
+  waybar -c ${full_path}/config.jsonc -s ${full_path}/style.css
 fi
